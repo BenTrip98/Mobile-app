9ac1f9e389f83b2860c55caa2d3208fa924b75fa
BenTrip98 <ben.tripaylan@gmail.com>
2022-09-19 05:47:29 -0300

    sas

---
+++ main.js
@@ -0,0 +1,9 @@
+const iconoMenu = document.querySelector("#icono-menu"),
+menu=document.querySelector("#menu")
+
+iconoMenu.addEventListener('click',(e)=>{
+    //alternar estilos para el menu y body
+    menu.classList.toggle("activo");
+    document.body.classList.toggle("opacity");
+
+})
