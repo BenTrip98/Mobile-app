906808b998aac19b2a1b1b0c77c4769b1ba6bc7e
BenTrip98 <ben.tripaylan@gmail.com>
2022-09-18 21:11:14 -0300

    yes

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ iconoMenu.addEventListener('click',(e)=>{
     //alternar estilos para el menu y body
     menu.classList.toggle("activo");
     document.body.classList.toggle("opacity");
-
+    
 })
