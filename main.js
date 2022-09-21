ed720e381c219fe10beb2139a1f73f8bf00c8a8d
BenTrip98 <ben.tripaylan@gmail.com>
2022-09-21 04:30:09 -0300

    terminar usuario

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
-const iconoMenu = document.querySelector("#icono-menu"),
+let iconoBoleano=false;
+const iconoUsuario = document.querySelector("#icono-usuario"),
 menu=document.querySelector("#menu")
 
-iconoMenu.addEventListener('click',(e)=>{
+iconoUsuario.addEventListener('click',(e)=>{
     //alternar estilos para el menu y body
     menu.classList.toggle("activo");
     document.body.classList.toggle("opacity");
-
+    iconoBoleano=!iconoBoleano
+    if(iconoBoleano===true){
+        iconoUsuario.className="fas fa-x";
+    }else{
+        iconoUsuario.className="fas fa-user";
+    }
+    
+    console.log(iconoBoleano)
+    
 })
